755e02613e7178576dba8c5b69189ab324feb730
Ehmad <justEhmadSaeed@gmail.com>
2020-04-21 01:53:10 +0500

    👌 IMPROVE: Include audioplayers package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +10,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Press Me'),
+            ),
+          ),
         ),
       ),
     );
